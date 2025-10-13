22bc07be6e540dc73218499d218e9d5e5e49a3b5
Jay Quigley <quigley.james.g@gmail.com>
2025-10-12 17:38:51 -0700

    fix app/components/MainMenu.unit.test.tsx

--- app/components/MainMenu.unit.test.tsx
+++ app/components/MainMenu.unit.test.tsx
@@ -1,16 +1,19 @@
-import { render, screen } from '@testing-library/react-native';
-import MainMenu from "./MainMenu"
+import { render, screen, waitFor } from '@testing-library/react-native';
+import MainMenu from './MainMenu';
 
-describe("MainMenu page", () => {
-    it("includes the appropriate buttons", () => {
-        const mockNavigate = () => {};
-        render(<MainMenu navigate={mockNavigate}/>);
+describe('MainMenu page', () => {
+  it('includes the appropriate buttons', async() => {
+    const mockNavigate = () => {};
 
-        const buttons = screen.getAllByRole("button")
-        expect(buttons.length).toBe(3);
-        
-        expect(screen.getByText("Flashcards")).toBeTruthy()
-        expect(screen.getByText("Fidels List")).toBeTruthy()
-        expect(screen.getByText("Stats")).toBeTruthy()
+    render(<MainMenu navigate={mockNavigate}/>);
+
+    await waitFor(() => {
+      const buttons = screen.getAllByRole('button');
+      expect(buttons.length).toBe(3);
+
+      expect(screen.getByText('Flashcards')).toBeTruthy();
+      expect(screen.getByText('Fidels List')).toBeTruthy();
+      expect(screen.getByText('Stats')).toBeTruthy();
     });
+  });
 });
